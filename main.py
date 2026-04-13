d4e287804a99f6f26286fcc87efdec9d49a9aede
GenesisNUP30 <jdirproy@gmail.com>
2026-04-13 23:24:32 +0200

    Habilitar opciones 3 y 4

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from core.gestor_datos import inicializar_sistema, guardar_analisis
 from interfaz.entradas import capturar_portapapeles, seleccionar_archivo_local, pedir_metadatos
 from core.analizador import limpiar_texto, obtener_sentimiento, generar_tags, obtener_top_palabras
 from interfaz.visual import crear_nube_palabras, mostrar_dashboard
+from core.estadisticas import obtener_resumen_temporal
+from core.gestor_datos import listar_artistas
 
 def mostrar_menu():
     print("\n" + "="*30)
@@ -49,10 +51,23 @@ def main():
                     print(f"\n[✓] Archivo guardado correctamente.")
 
         elif opcion == "3":
-            print("\n[Próximamente] Navegación por biblioteca...")
+            artistas = listar_artistas()
+            if not artistas:
+                print("\n[!] Tu biblioteca está vacía.")
+            else:
+                print("\n--- Artistas en tu Biblioteca ---")
+                for i, art in enumerate(artistas, 1):
+                    print(f"{i}. {art}")
+                    # Se podria añadir lógica para analizar la carpeta entera
 
         elif opcion == "4":
-            print("\n[Próximamente] Estadísticas temporales...")
+            print("\n¿Qué periodo quieres consultar?")
+            print("1. Última semana (7 días)")
+            print("2. Último mes (30 días)")
+            sub_opcion = input("Elige: ")
+            
+            dias = 7 if sub_opcion == "1" else 30
+            print(obtener_resumen_temporal(dias))
 
         elif opcion == "5":
             print("\n¡Gracias por usar LyricsVibe! Hasta pronto.")
